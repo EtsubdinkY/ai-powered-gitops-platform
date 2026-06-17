967a68388d60a426175337063c24cbfac71f2f93
Etsub Y <etsuby@mac.mynetworksettings.com>
2026-06-17 12:57:30 -0400

    Use Workload Identity credentials for Terraform provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -12,6 +12,4 @@ terraform {
 provider "google" {
   project = "project-ff80b6ff-8c4e-4127-89e"
   region  = "us-central1"
-
-  impersonate_service_account = "terraform-deployer@project-ff80b6ff-8c4e-4127-89e.iam.gserviceaccount.com"
 }
