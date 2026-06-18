45107d51a75444b6695ed8062fe43028d6b99071
Etsub Y <etsuby@mac.mynetworksettings.com>
2026-06-17 20:55:51 -0400

    Add test VPC

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,4 @@
-resource "google_storage_bucket" "terraform_test" {
-  name                        = "terraform-test-bucket-75857734333"
-  location                    = "US"
-  force_destroy               = true
-  uniform_bucket_level_access = true
+resource "google_compute_network" "terraform_test" {
+  name                    = "terraform-test-vpc"
+  auto_create_subnetworks = false
 }
