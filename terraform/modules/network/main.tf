bfb95be2803aa561c7abfe5fbf042775b8507a78
Etsub Y <etsuby@mac.mynetworksettings.com>
2026-06-23 20:02:03 -0400

    Add subnet logging and firewall rules

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -9,6 +9,14 @@ resource "google_compute_subnetwork" "this" {
   network       = google_compute_network.this.id
   ip_cidr_range = var.subnet_cidr
 
+  private_ip_google_access = true
+
+  log_config {
+    aggregation_interval = "INTERVAL_5_SEC"
+    flow_sampling        = 0.5
+    metadata             = "INCLUDE_ALL_METADATA"
+  }
+
   secondary_ip_range {
     range_name    = var.pods_range_name
     ip_cidr_range = var.pods_cidr
@@ -17,5 +25,30 @@ resource "google_compute_subnetwork" "this" {
   secondary_ip_range {
     range_name    = var.services_range_name
     ip_cidr_range = var.services_cidr
+  }
+}
+
+resource "google_compute_firewall" "allow_internal" {
+  name    = "${var.network_name}-allow-internal"
+  network = google_compute_network.this.name
+
+  direction = "INGRESS"
+
+  source_ranges = [
+    var.subnet_cidr,
+    var.pods_cidr,
+    var.services_cidr
+  ]
+
+  allow {
+    protocol = "tcp"
+  }
+
+  allow {
+    protocol = "udp"
+  }
+
+  allow {
+    protocol = "icmp"
   }
 }
